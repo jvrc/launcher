f9dc1ce4898633992e5aa7fd1eeacf0662945c22
Kin-ichi WATANABE <kin@soum.co.jp>
2015-10-01 14:52:06 +0900

    merges R2AB + R2AB_2, R2C + R2C_2 tasks.

--- Task.R2C_2/50cnoid_exec.sh
+++ Task.R2C_2/50cnoid_exec.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
 
-_taskname="R2C_2"
+_taskname="R2C"
+_taskpoint="_2"
 _top_dir="/media/player/JVRC4GU"
 
 task_exec_cmd="${_top_dir}/model/tasks/task_exec.sh"
 
 ###
-/bin/bash -x ${task_exec_cmd} ${_top_dir} ${_taskname}
+/bin/bash -x ${task_exec_cmd} ${_top_dir} ${_taskname} ${_taskpoint}
